d9762607ab698c1839321ab06d45f7e94b55a0c9
mbolonzi-design <mbolonzi.design@gmail.com>
2022-08-30 08:48:33 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 9, 'M', 'Black', 'Playful', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Brown", "Playful", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "White", "Joyful", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Black", "Social", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "White", "Playful", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "F", "Brown", "Angry", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "White", "Altruisic", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (Null, Null, "M", "Black", "Angry", 1);
